1b428ae2c84a6845345aeca40b4128e0d3d4d62e
vithxrlorencetti <vithorlorencetti@gmail.com>
2022-05-04 21:34:06 -0300

    paciente service criada

--- src/main/java/br/com/uniamerica/api/repository/PacienteRepository.java
+++ src/main/java/br/com/uniamerica/api/repository/PacienteRepository.java
@@ -2,6 +2,9 @@ package br.com.uniamerica.api.repository;
 
 import br.com.uniamerica.api.entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -11,4 +14,11 @@ import org.springframework.stereotype.Repository;
  * @version 1.0.0
  */
 @Repository
-public interface PacienteRepository extends JpaRepository<Paciente, Long> { }
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    @Modifying
+    @Query("UPDATE Paciente paciente " +
+            "SET paciente.excluido = now() " +
+            "WHERE paciente.id = :paciente")
+    public void updateStatus(@Param("paciente") Long idPaciente);
+}
---
+++ src/main/java/br/com/uniamerica/api/service/PacienteService.java
@@ -0,0 +1,83 @@
+package br.com.uniamerica.api.service;
+
+import br.com.uniamerica.api.entity.Paciente;
+import br.com.uniamerica.api.entity.Secretaria;
+import br.com.uniamerica.api.entity.TipoAtendimento;
+import br.com.uniamerica.api.repository.PacienteRepository;
+import br.com.uniamerica.api.repository.SecretariaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public class PacienteService {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public Optional<Paciente> findById(Long id){
+        return this.pacienteRepository.findById(id);
+    }
+
+    public Page<Paciente> listAll(Pageable pageable){
+        return this.pacienteRepository.findAll(pageable);
+    }
+
+    public void insert(Paciente paciente){
+        this.validarFormulario(paciente);
+        this.saveTransactional(paciente);
+    }
+
+    public void update(Long id, Paciente paciente){
+        if (id == paciente.getId()){
+            this.validarFormulario(paciente);
+            this.saveTransactional(paciente);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
+    @Transactional
+    public void updateStatus(Long id, Paciente paciente){
+        if (id == paciente.getId()) {
+            this.pacienteRepository.updateStatus(paciente.getId());
+        }
+        else {
+            throw new RuntimeException();
+        }
+    }
+
+    @Transactional
+    public void saveTransactional(Paciente paciente){
+        this.pacienteRepository.save(paciente);
+    }
+
+    public void validarFormulario(Paciente paciente){
+
+        if(paciente.getTipoAtendimento().equals(TipoAtendimento.convenio)){
+            if (paciente.getConvenio().getId() == null){
+                throw new RuntimeException("Convenio não informado.");
+            }
+            if (paciente.getNumeroCartaoConvenio() == null){
+                throw new RuntimeException("Numero do Cartão do Convenio não informado.");
+            }
+            if (paciente.getDataVencimento() == null){
+                throw new RuntimeException("Data de Vencimento do Cartão não informada.");
+            }
+            if (paciente.getDataVencimento().compareTo(LocalDateTime.now()) > 0){
+                throw new RuntimeException("Data de Vencimento do Cartão expirada.");
+            }
+        }
+
+        if (paciente.getTipoAtendimento().equals(TipoAtendimento.particular)){
+            paciente.setConvenio(null);
+            paciente.setNumeroCartaoConvenio(null);
+            paciente.setDataVencimento(null);
+        }
+
+    }
+
+}
