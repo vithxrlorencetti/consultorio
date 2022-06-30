7453a327bd3c660c44e91f4eaba44d30ed5f1b15
vithxrlorencetti <vithorlorencetti@gmail.com>
2022-06-29 21:58:24 -0300

    listar funcional

--- src/main/java/br/com/uniamerica/api/controller/EspecialidadeController.java
+++ src/main/java/br/com/uniamerica/api/controller/EspecialidadeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/api/especialidades")
 public class EspecialidadeController {
 
